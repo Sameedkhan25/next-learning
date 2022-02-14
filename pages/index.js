6d053d0fef447f94e3fbccec214acd490deeefda
Sameed <msameedkhan25@gmail.com>
2022-02-15 03:27:01 +0500

    adding 404 page

--- pages/index.js
+++ pages/index.js
@@ -2,8 +2,15 @@
 import styles from '../styles/Home.module.css'
 
 import Link from 'next/link'
+import { useRouter } from 'next/router'
 
 export default function Home() {
+  const router= useRouter()
+  const handleClick = () => {
+    console.log('clicked')
+  
+    router.push('/feature/featureOne')
+  }
   
   return (
     <div className={styles.container}>
@@ -11,7 +18,13 @@ export default function Home() {
      <Link href='/feature' replace>
         <a>feature</a>
      </Link>
+
+      <button onClick={handleClick}>
+        go to feature page
+      </button>
+     
     </div>
   )
+    
 }
 //replace: on back button, it will replace the current page with the homepage not the previous page
